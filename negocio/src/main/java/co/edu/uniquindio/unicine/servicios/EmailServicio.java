4c401e8478432753136454776aa46ef30587091f
unknown <jenunezb@uqvirtual.edu.co>
2022-11-20 18:50:00 -0500

    inicio de formularios

--- negocio/src/main/java/co/edu/uniquindio/unicine/servicios/EmailServicio.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/servicios/EmailServicio.java
@@ -1,17 +1,21 @@
 package co.edu.uniquindio.unicine.servicios;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+
 import javax.mail.internet.MimeMessage;
 
 @Service
+@Configuration
 public class EmailServicio {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
 
     public boolean enviarEmail(String asunto, String contenido, String destinatario){
 
--- negocio/src/test/java/co/edu/uniquindio/unicine/test/EmailTest.java
+++ negocio/src/test/java/co/edu/uniquindio/unicine/test/EmailTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 
 import javax.transaction.Transactional;
 
